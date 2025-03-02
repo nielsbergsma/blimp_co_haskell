6c63ce17a5cc4c3f3e73d359d4d32fa5d9e26e37
Niels Bergsma <n.bergsma@gmail.com>
2025-03-02 20:17:37 +0100

    disabled hover on touch centric devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/**/*.{hs,html,js}", "./index.html"],
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   theme: {
     extend: {},
   },
